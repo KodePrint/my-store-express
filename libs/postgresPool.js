d9134537fc1cca99e393ac8f51cc61caf79687f5
Kevin Alberto Palma Ralda <kapalma05@gmail.com>
2022-03-21 12:17:57 -0600

    :sparkles: Modify a pool
    Modify a pool

--- libs/postgresPool.js
+++ libs/postgresPool.js
@@ -1,10 +1,16 @@
 const { Pool } = require('pg');
 const {config} = require('../config/config');
 
-const USER = encodeURIComponent(config.dbUser)
-const PASSWORD = encodeURIComponent(config.dbPassword)
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const options = {};
 
-const pool = new Pool({connectionString: URI})
+if (config.isProd) {
+    options.connectionString = config.dbUrl;
+    optons.dialectOptions: {
+	ssl: {rejectUnauthorized:false}
+    }
+}
+
+
+const pool = new Pool(options);
 
 module.exports = pool;
